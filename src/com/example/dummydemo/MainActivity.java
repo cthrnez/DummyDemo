7096541c87f789e7f904a74efc201461a353f6f9
Catherine <catherine@31-33-229.wireless.csail.mit.edu>
2014-12-09 16:43:44 -0500

    Save passed byte array to local file?

--- src/com/example/dummydemo/MainActivity.java
+++ src/com/example/dummydemo/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.dummydemo;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
@@ -8,16 +11,16 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends Activity {
-    private BroadcastReceiver myReceiver;
+	private BroadcastReceiver myReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("HAHAHHAA");
+        
+        // Create a Broadcast Receiver for receiving the ticket.  
         IntentFilter filter = new IntentFilter();
         filter.addAction("com.example.dummydemo.TESTING");
         myReceiver = new BroadcastReceiver() {
@@ -27,6 +30,21 @@ public class MainActivity extends Activity {
     			Log.d("Debug", "Final receiver");
     			String s = intent.getExtras().getString("ticket");
     			System.out.println(s);
+    			// Get ticket as array of bytes.
+    			byte [] binary = intent.getExtras().getByteArray("bytes");
+    			// Save ticket in local storage?  
+    			String filename="blah";
+    			try {
+    				System.out.println("Starting to write byte array.");
+    		        FileOutputStream fos= openFileOutput(filename, Context.MODE_PRIVATE);
+    		        fos.write(binary);
+    		        fos.flush();
+    		        fos.close();
+    		        System.out.println("Done writing byte array.");
+    		      }
+    		      catch (java.io.IOException e) {
+    		        Log.e("DemoError", "Exception in saving byte array to local", e);
+    		      }
     		}
     	};
     	registerReceiver(myReceiver, filter, "com.example.dummyKerb.LISTEN_PERM", null);
@@ -41,25 +59,6 @@ public class MainActivity extends Activity {
         //System.out.println("Packname " + pname);
         intent.putExtra("package", pname);
         sendBroadcast(intent, "com.example.dummyKerb.LISTEN_PERM");
-    }
-
-    /* method to send intent to getTicket */
-    public void getTickets(View view){
-        Intent intent = new Intent();
-    	intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
-        intent.setAction("com.example.dummyKerb.TESTING");
-        //intent.putExtra("package", "com.example.dummydemo");
-        Context c = this.getApplicationContext();
-        String pname = c.getPackageName();
-        System.out.println("Packname " + pname);
-        intent.putExtra("package", pname);
-        sendBroadcast(intent, "com.example.dummyKerb.LISTEN_PERM");
-    }
-    
-	/* Create a Broadcast Receiver for receiving the ticket. 
-	 * This method also starts display ticket activity*/
-   
-    public void receiveTicket(){
     }
 
 
